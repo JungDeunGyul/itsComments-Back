0da619e6ec897e95d91b47df4e8f733ff76425a9
sunghankwon <akvo852@naver.com>
2024-02-05 21:35:15 +0900

    chore: Add CORS allowed URL

--- routes/login.js
+++ routes/login.js
@@ -1,22 +1,27 @@
 const express = require("express");
+
 const router = express.Router();
 
 <<<<<<< HEAD
 const { User } = require("../Models/User");
 
+<<<<<<< HEAD
 =======
 >>>>>>> 58e3390 (feat: create login router and verifyToken middleware and add cors)
 router.post("/", async (req, res, next) => {
+=======
+router.post("/", async (req, res) => {
+>>>>>>> 32bfb0b (style:Standardize response format to use JSON)
   try {
     const userData = req.body.user;
 
     res.cookie("accessToken", userData.stsTokenManager.accessToken);
     res.cookie("refreshToken", userData.stsTokenManager.refreshToken);
 
-    const user = await User.findOne({ email: userData.email });
+    let user = await User.findOne({ email: userData.email });
 
     if (!user) {
-      User.create({
+      user = await User.create({
         email: userData.email,
         nickname: userData.displayName,
 <<<<<<< HEAD
@@ -30,9 +35,9 @@ router.post("/", async (req, res, next) => {
       });
     }
 
-    res.send({ result: "success" });
+    res.status(200).json({ user });
   } catch (error) {
-    return res.send({ result: "fail", message: "Login failed" });
+    return res.status(400).json({ message: "Login failed." });
   }
 });
 
